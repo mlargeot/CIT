ba96c6b0e8625eba6c286e2954ef6b551af1c821
mlargeot <morgan.largeot@epitech.eu>
2024-05-01 10:14:23 +0200

    feat(build): Add docker installation & start docker project

--- build.sh
+++ build.sh
@@ -24,13 +24,13 @@ if ! command -v crontab &> /dev/null; then
         apt-get update
         apt-get install cron
     else
-        echo -e "[${Red}KO${Color_Off}] Crontab installation failed due to package manager."
+        echo -e "[${Red}KO${Color_Off}] Crontab installation failed. Unsupported package manager."
         exit 1
     fi
 
     # Handle installation state
     if command -v crontab &> /dev/null; then
-        echo -e "[${Green}OK${Color_Off}] Crontab installation."
+        echo -e "[${Green}OK${Color_Off}] Crontab installation successful."
     else
         echo -e "[${Red}KO${Color_Off}] Crontab installation failed."
     fi
@@ -51,18 +51,46 @@ if ! command -v python3 &> /dev/null; then
         apt-get update
         apt-get install python3
     else
-        echo -e "[${Red}KO${Color_Off}] Python3 installation failed due to package manager."
+        echo -e "[${Red}KO${Color_Off}] Python3 installation failed. Unsupported package manager."
         exit 1
     fi
 
     # Handle Python3 installation state
     if command -v python3 &> /dev/null; then
-        echo -e "[${Green}OK${Color_Off}] Python3 installation."
+        echo -e "[${Green}OK${Color_Off}] Python3 installation successful."
     else
         echo -e "[${Red}KO${Color_Off}] Python3 installation failed."
     fi
 else
     echo -e "[${Green}OK${Color_Off}] Python3 already installed."
+fi
+
+# Install Docker
+echo -e "${Yellow}[=================> Docker Installation <=================]${Color_Off}"
+if ! command -v docker &> /dev/null; then
+    echo -e "[${Red}KO${Color_Off}] Docker not found. Installing Docker..."
+    
+    # Install Docker on Ubuntu
+    if command -v apt-get &> /dev/null; then
+        apt-get update
+        apt-get install docker.io
+    # Install Docker on Arch Linux
+    elif command -v pacman &> /dev/null; then
+        pacman -Sy docker --noconfirm
+    else
+        echo -e "[${Red}KO${Color_Off}] Docker installation failed. Unsupported package manager."
+        exit 1
+    fi
+
+    # Check Docker installation state
+    if command -v docker &> /dev/null; then
+        echo -e "[${Green}OK${Color_Off}] Docker installation successful."
+    else
+        echo -e "[${Red}KO${Color_Off}] Docker installation failed."
+        exit 1
+    fi
+else
+    echo -e "[${Green}OK${Color_Off}] Docker already installed."
 fi
 
 APP_DIR=/app/cit
@@ -78,6 +106,26 @@ else
     echo -e "[${Green}OK${Color_Off}] ${APP_DIR} directory already exists."
 fi
 
+if [ -f "$(dirname "$0")/.env" ]; then
+        cp "$(dirname "$0")/.env" "$APP_DIR"
+        echo -e "[${Green}OK${Color_Off}] Copied .env file to ${APP_DIR}."
+else
+        echo -e "[${Red}WARNING${Color_Off}] No .env file found in the source directory."
+fi
+
+echo -e "${Yellow}[=================> Building and Starting Docker Project <=================]${Color_Off}"
+
+cd "$APP_DIR"
+docker-compose up -d --build
+
+# Check Docker Compose execution status
+DOCKER_COMPOSE_EXIT_CODE=$?
+if [ $DOCKER_COMPOSE_EXIT_CODE -eq 0 ]; then
+    echo -e "[${Green}OK${Color_Off}] Docker project running"
+else
+    echo -e "[${Red}KO${Color_Off}] Docker project failed. See logs"
+fi
+
 # Cron job setup for script that will run every 5 minutes
 echo -e "${Yellow}[=================> Setting up Cron Job for script <=================]${Color_Off}"
 if command -v crontab &> /dev/null; then
@@ -89,7 +137,9 @@ if command -v crontab &> /dev/null; then
     crontab /tmp/cron_job
     rm /tmp/cron_job
 
-    echo -e "[${Green}OK${Color_Off}] Scheduled main.py to run every 5 minutes using crontab."
+    echo -e "[${Green}OK${Color_Off}] Crontab setup"
 else
-    echo -e "[${Red}KO${Color_Off}] Failed to set up cron job for main.py. Crontab not found."
+    echo -e "[${Red}KO${Color_Off}] Crontab setup"
 fi
+
+echo -e "${Green}Build completed successfully.${Color_Off}"
