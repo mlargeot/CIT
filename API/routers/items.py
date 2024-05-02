fa6407119d66bca86857a18e043754fa3cb7a0a6
mlargeot <morgan.largeot@epitech.eu>
2024-05-02 14:06:10 +0200

    feat(API): Add GET function for items route

--- API/routers/items.py
+++ API/routers/items.py
@@ -9,6 +9,30 @@ router = APIRouter(prefix='/items')
 
 items = []
 
-@router.get("/")
-def get_items():
+@router.get('/', status_code=200)
+async def get_items():
+
+    try:
+        binance_request = "https://www.binance.com/api/v3/ticker/price"
+        binance_response = requests.get(binance_request)
+    except:
+        raise HTTPException(status_code=500, detail='Unable to get ticker price')
+
+    try:
+        conversion_request = "https://cdn.taux.live/api/latest.json"
+        conversion_response = requests.get(conversion_request)
+        convertion_rate = conversion_response.json()['rates']['EUR']
+    except:
+       raise HTTPException(status_code=500, detail='Unable to load conversion currency')
+
+    try:
+        for item in binance_response.json():
+            newItem: Item = Item()
+            newItem.symbol = item["symbol"]
+            newItem.value_usd = item["price"]
+            newItem.value_eur = str(float(item["price"]) * float(convertion_rate))
+            items.append(newItem)
+    except:
+        raise HTTPException(status_code=500, detail='Error while loading json data')
+
     return list_to_dict(items)
