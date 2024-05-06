4be1876d9e8bf899b9418a9c9e3f5bb7aa2dcd88
mlargeot <morgan.largeot@epitech.eu>
2024-05-06 10:35:37 +0200

    feat(main): Add basic update of excel sheet

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import sys
 import requests
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
 
 def add_filter_elm(symbol: str) -> bool:
     try:
@@ -18,6 +20,29 @@ def del_filter_elm(symbol: str) -> bool:
     return True
 
 def update_excel() -> bool:
+    try:
+        scope = ['https://www.googleapis.com/auth/spreadsheets', "https://www.googleapis.com/auth/drive.file", "https://www.googleapis.com/auth/drive"]
+        credentials = ServiceAccountCredentials.from_json_keyfile_name('./test.json', scope)
+        client = gspread.authorize(credentials)
+        google_sheet = client.open('Testsheet').sheet1
+    except:
+        return False
+    
+    try:
+        filter_content = requests.get('http://127.0.0.1:8000/api/filter/')
+    except:
+        return False
+
+    try:
+        col = 2
+        row = 2
+        for elm in filter_content.json():
+            google_sheet.update_cell(row, col, str(filter_content.json()[elm]['symbol']))
+            google_sheet.update_cell(row + 1, col, float(filter_content.json()[elm]['value_usd']))
+            google_sheet.update_cell(row + 2, col, float(filter_content.json()[elm]['value_eur']))
+            col += 1
+    except:
+        return False
     return True
 
 def main():
